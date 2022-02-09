774ba2514d9977cb246468f70c9b657a48de5d5c
LuboDimitrov <lubo9991@hotmail.com>
2022-02-09 19:21:57 +0100

    Service layer added

---
+++ Simple_project/src/main/java/com/example/Simple_project/Student/StudentService.java
@@ -0,0 +1,23 @@
+package com.example.Simple_project.Student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.util.List;
+
+/*Class that will serve as Business logic for managing students
+ * So service layer is mainly responsible for business logic*/
+
+public class StudentService {
+
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        10L,
+                        "john@gmail.com",
+                        "john",
+                        LocalDate.of(1999, Month.JUNE, 7),
+                        Period.between((LocalDate.of(1999, Month.JUNE, 7)),LocalDate.now()).getYears())
+        );
+    }
+}
